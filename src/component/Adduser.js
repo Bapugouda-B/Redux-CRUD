4625fe5274c89896fe71c9a9b79203374eea1f62
Bapugouda <rajjdanush@gmail.com>
2024-03-20 18:22:50 +0530

    Created Add Student Page and consoled the student data using handleSubmit function

---
+++ src/component/Adduser.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Adduser() {
+  const [name, setName] = useState("");
+  const [father, setFather] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [role, setRole] = useState("Science");
+
+  const studentObject = { name, father, email, phone, role };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(studentObject);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="card">
+          <div className="card-header" style={{ textAlign: "left" }}>
+            <h2>Add Student</h2>
+          </div>
+          <div className="card-body" style={{ textAlign: "left" }}>
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Student Name</label>
+                  <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Father Name</label>
+                  <input
+                    value={father}
+                    onChange={(e) => setFather(e.target.value)}
+                    type="text"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Email</label>
+                  <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="text"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Phone</label>
+                  <input
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    type="text"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <label>Role</label>
+                  <select
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
+                    className="form-control"
+                  >
+                    <option value="Class Leader">Sceince</option>
+                    <option value="Class Leader">Commerce</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card-footer" style={{ textAlign: "left" }}>
+            <button className="btn btn-primary">Submit</button>
+            <Link className="btn btn-danger">Back</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Adduser;
